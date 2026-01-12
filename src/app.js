9655baeaf541bff8a2bd7e76d3a91566b4b77563
Manivass <manivassv@gmail.com>
2026-01-12 18:54:43 +0530

    using regex in routing

--- src/app.js
+++ src/app.js
@@ -2,29 +2,21 @@ const express = require("express");
 
 const app = express();
 
-// app.use("/hello", (req , res) => res.send("Hello for the server"));
-
-// app.use("/hellob" , (req , res) => res.send("Dubokur Manda")) ;
-
-app.use("/test", (req, res) => res.send("HAHAHAHAHAHAHAHA"));
-
-app.post("/test", (req, res) => {
-  res.end(" Successfully Posted ");
+app.use(/^\/ab?c$/, (req, res) => {
+  res.send("b is optional ");
 });
 
-app.get("/test", (req, res) => {
-  res.end(" Successfully tested ");
+app.use(/^\/ab+c$/, (req, res) => {
+  res.send("b can comes multiple times");
 });
 
-app.put("/test", (req, res) => {
-  res.end("Successfully updated..");
+app.use(/^\/ab*cd$/, (req, res) => {
+  res.send("anything come between ab and cd");
 });
 
-app.delete("/test", (req, res) => {
-  res.end("successfully deleted.");
+app.use(/.*fly$/, (req, res) => {
+  res.end("a will come");
 });
-
-// app.use("/", (req, res) => res.send(" Creating an Dev Tinder Project "));
 
 app.listen(4000, () => {
   console.log("Server Successfully Connected to Server");
