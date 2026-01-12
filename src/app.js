ffc6e6ddba4a1dccd8ca81e1dff4831e3e8c2de2
Manivass <manivassv@gmail.com>
2026-01-12 17:33:20 +0530

    exploring get , post , put , delete

--- src/app.js
+++ src/app.js
@@ -2,11 +2,29 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test", (req, res) => res.send(" Testing the DevTinder Project"));
+// app.use("/hello", (req , res) => res.send("Hello for the server"));
 
-app.use("/hello", (req , res) => res.send("Hello for the serverf"));
+// app.use("/hellob" , (req , res) => res.send("Dubokur Manda")) ;
 
-app.use("/", (req, res) => res.send(" Creating an Dev Tinder Project "));
+app.use("/test", (req, res) => res.send("HAHAHAHAHAHAHAHA"));
+
+app.post("/test", (req, res) => {
+  res.end(" Successfully Posted ");
+});
+
+app.get("/test", (req, res) => {
+  res.end(" Successfully tested ");
+});
+
+app.put("/test", (req, res) => {
+  res.end("Successfully updated..");
+});
+
+app.delete("/test", (req, res) => {
+  res.end("successfully deleted.");
+});
+
+// app.use("/", (req, res) => res.send(" Creating an Dev Tinder Project "));
 
 app.listen(4000, () => {
   console.log("Server Successfully Connected to Server");
