4d40e53b1b40cacb55a4c05d41ec7fabd9ae50f9
Manivass <manivassv@gmail.com>
2026-01-13 08:55:59 +0530

    creating multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -2,15 +2,29 @@ const express = require("express");
 
 const app = express();
 
-app.use("/user", (req, res) => {
-  console.log(req.query);
-  res.end("Get the user Search params");
-});
-
-app.use("/texter/:name/:pass", (req, res) => {
-  console.log(req.params);
-  res.end("Get the user params");
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("Creating a Header 1 ");
+    // res.end("1st Resopnse");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Creating a Header 2");
+    // res.end("2nd Response");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Creating a Header 3");
+    // res.end("3rd Response");
+    next();
+  },
+  (req, res , next) => {
+    console.log("Creating a Header 4");
+    res.end("4th Response");
+    //next() => it shown an error , last route handler must doesnt want next() 
+  }
+);
 
 app.listen(4000, () => {
   console.log("Server Successfully Connected to Server");
